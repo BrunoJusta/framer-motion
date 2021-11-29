351d78d697de13431ae5c993df48043498a2643b
BrunoJusta <brunomajusta@gmail.com>
2021-11-29 15:59:48 +0000

    useEffect

--- components/Loader/Loader.js
+++ components/Loader/Loader.js
@@ -1,50 +1,66 @@
-import { motion } from "framer-motion";
-import useWindowSize from "../../hooks/Dimensions";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState, useEffect } from "react";
 
 const Loader = (props) => {
   const { size } = props;
   const [height, setHeight] = useState(null);
+  const [show, setShow] = useState(true);
+  const [isAnimatig, setIsAnimating] = useState(null);
+
+  const loaderVariants = {
+    moveBlack: {
+      x: [-2000, 0, 2600],
+      transition: { duration: 2, ease: "easeInOut" },
+    },
+    moveWhite: {
+      x: [0, 0, 2600],
+      transition: { duration: 2, ease: "easeInOut" },
+    },
+  };
   useEffect(() => {
     if (size) {
       setHeight(size.current.getBoundingClientRect().height);
-      console.log("wow");
     }
-  });
-  const windowSize = useWindowSize();
+    setIsAnimating(true);
+    setTimeout(() => {
+      setShow(false);
+      setIsAnimating(false);
+    }, 2100);
+  }, []);
+
   return (
-    <>
-      <motion.div
-        style={{
-          backgroundColor: "#000",
-          width: "100%",
-          height: `${height}px`,
-          position: "absolute",
-          zIndex: "100",
-        }}
-        animate={{
-          x: [-2000, 0, 2600],
-          opacity: [1, 1, 1, 0],
-        }}
-        transition={{ duration: 2, ease: "easeInOut" }}
-      ></motion.div>
-      <motion.div
-        style={{
-          backgroundColor: "#fff",
-          width: "100%",
-          height: `${height}px`,
-          position: "absolute",
-          zIndex: "80",
-          top: 0,
-          left: 0,
-        }}
-        animate={{
-          x: [0, 0, 2600],
-          opacity: [1, 1, 1, 0],
-        }}
-        transition={{ duration: 2, ease: "easeInOut" }}
-      ></motion.div>
-    </>
+    <div>
+      {show ? (
+        <motion.div
+          style={{
+            backgroundColor: "#202020",
+            width: "100%",
+            height: `${height}px`,
+            position: "absolute",
+            zIndex: "100",
+          }}
+          variants={loaderVariants}
+          animate={isAnimatig ? "moveBlack" : ""}
+        ></motion.div>
+      ) : (
+        ""
+      )}
+      {show ? (
+        <motion.div
+          style={{
+            backgroundColor: "#fafafa",
+            width: "100%",
+            height: `${height}px`,
+            position: "absolute",
+            zIndex: "80",
+          }}
+          variants={loaderVariants}
+          animate={isAnimatig ? "moveWhite" : ""}
+        ></motion.div>
+      ) : (
+        ""
+      )}
+    </div>
   );
 };
 
